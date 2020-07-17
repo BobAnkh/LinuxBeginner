7dac2721a16927769514e13be4562d72b5a2dd92
jwwwb <2066729323@qq.com>
2020-07-17 11:12:48 +0800

    docs(md-pdf.sh): adjust the code
    - save pdf files in /pdfs instead of /pdfs
    - convert markdown files in docs/{LinuxCommand, LinuxTool} only

--- scripts/md-pdf.sh
+++ scripts/md-pdf.sh
@@ -5,29 +5,28 @@ export PATH=$PATH:$HOME/bin #将Tex相关可执行文件添加到PATH
 #安装其他一些必要的Tex包
 tlmgr install unicode-math filehook xecjk xltxtra realscripts fancyhdr lastpage ctex ms cjk ulem environ trimspaces zhnumber collection-fontsrecommended
 #遍历repo下的文件，将所有md文件转换到$GITHUB_WORKSPACE/PDFs路径下变成pdf文件
-PDF_PATH=$GITHUB_WORKSPACE/PDFs
+DOC_PATH=$GITHUB_WORKSPACE/docs
+PDF_PATH=$GITHUB_WORKSPACE/pdfs
 trave_conv(){
-    for file in `ls $GITHUB_WORKSPACE/$1`
+    for file in `ls $DOC_PATH/$1`
     do
-        if [ $file != PDFs ]
+        if [ -f $DOC_PATH/$1/$file ]
         then
-            if [ -f $GITHUB_WORKSPACE/$1/$file ]
+            if [[ $file =~ '.md' ]]
             then
-                if [[ $file =~ '.md' ]]
+                if [ ! -d $PDF_PATH/$1 ]
                 then
-                    if [ ! -d $PDF_PATH/$1 ]
-                    then
-                        mkdir -p $PDF_PATH/$1
-                    fi
-                    echo "-----NOW CONVERTING"$1/$file"-----"
-                    pandoc $GITHUB_WORKSPACE/$1/$file -o $PDF_PATH/$1/${file//'.md'/'.pdf'} --pdf-engine=xelatex -V mainfont='PingFang SC' --template=$GITHUB_WORKSPACE/scripts/template.tex
+                    mkdir -p $PDF_PATH/$1
                 fi
-            else
-                trave_conv $1/$file
+                echo "-----NOW CONVERTING"$1/$file"-----"
+                pandoc $DOC_PATH/$1/$file -o $PDF_PATH/$1/${file//'.md'/'.pdf'} --pdf-engine=xelatex -V mainfont='PingFang SC' --template=$GITHUB_WORKSPACE/scripts/template.tex
             fi
+        else
+            trave_conv $1/$file
         fi
     done
 }   
 rm -rf $PDF_PATH
 mkdir -p $PDF_PATH
-trave_conv /
+trave_conv LinuxCommand
+trave_conv LinuxTool
