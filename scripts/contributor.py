e059d8a8d715b5f96527c2966f3add1eb6429477
BobAnkh <bobankhshen@gmail.com>
2020-07-22 16:14:44 +0800

    chore(contrib): update contrib workflow

--- scripts/contributor.py
+++ scripts/contributor.py
@@ -4,11 +4,13 @@ import re
 import base64
 
 ACCESS_TOKEN = ''
-REPO_NAME = 'BobAnkh/LinuxBeginner'
-CONTRIB = '### 贡献者\n\n'
-CONTRIB_END = '\n\n## 使用许可'
+REPO_NAME = ''
+CONTRIB = ''
 COLUMN_PER_ROW = 6
 ACCESS_TOKEN = sys.argv[1]
+REPO_NAME = sys.argv[2]
+CONTRIB = sys.argv[3] + '\n\n'
+COLUMN_PER_ROW = int(sys.argv[4])
 
 g = Github(ACCESS_TOKEN)
 repo = g.get_repo(REPO_NAME)
@@ -29,8 +31,9 @@ for contributor in contributors:
     if name == None:
         name = html_url[19:]
     if USER >= COLUMN_PER_ROW:
-        new_tr = '''\n<tr>\n<tr>'''
+        new_tr = '''\n</tr>\n<tr>'''
         head = head + new_tr
+        USER = 0
     td = f'''
     <td align="center">
         <a href={html_url}>
@@ -49,10 +52,15 @@ base = contents.content
 base = base.replace('\n', '')
 text = base64.b64decode(base).decode('utf-8')
 str = text.split(CONTRIB)
-end = str[1].split(CONTRIB_END)
+if tail in str[1]:
+    end = str[1].split(tail)
+    end[0] = end[0] + tail
+else:
+    end = ['', str[1]]
+
 if end[0] != head:
     end[0] = head
-    text = str[0] + CONTRIB + end[0] + CONTRIB_END + end[1]
+    text = str[0] + CONTRIB + end[0] + end[1]
     repo.update_file(contents.path, "docs(contrib): update contributors", text,
                      contents.sha)
     repo.update_file(contents_bkp.path, "docs(contrib): update contributors",
